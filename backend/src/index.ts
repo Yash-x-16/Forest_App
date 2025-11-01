9fac3441fb614378701b960828cef91359fe696a
Yash Mishra <mishrayash392@gmail.com>
2025-11-01 11:35:45 +0530

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,10 @@
+console.log('radhe radhe :)')
+import express from "express" 
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express() 
+
+app.listen(process.env.PORT,()=>{
+    console.log(`port is running on port ${process.env.PORT}`)
+}) 
