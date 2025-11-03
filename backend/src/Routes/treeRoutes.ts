61fac50e2d74148b8cc20b39a795efa921bd26cc
Yash Mishra <mishrayash392@gmail.com>
2025-11-03 19:33:23 +0530

    completed TreeRoutes

--- backend/src/Routes/treeRoutes.ts
+++ backend/src/Routes/treeRoutes.ts
@@ -1,11 +1,16 @@
 import express from "express" 
-import { addTree } from "../Controller/treeController.js"
+import { addTree, purchaseTree } from "../Controller/treeController.js"
+import { Middleware } from "../middleware/middleware.js";
 
 const router  = express.Router() 
 
 try {
-    router.post('/createTree',addTree) ; 
+    
+    router.post('/createTree',Middleware,addTree) ; 
+    router.post('/buyTree',Middleware,purchaseTree)  ;
+
 } catch (error) {
     console.log(error)
 }
+
 export default router ; 
